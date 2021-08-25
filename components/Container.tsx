47dc86fcdbeb10890181c3c6f2000a5dac0956e3
Kishan Patel <kpatel20538@gmail.com>
2021-08-25 14:07:34 -0500

    Added keyboard controllers to mode changing actions

--- components/Container.tsx
+++ components/Container.tsx
@@ -3,11 +3,13 @@ import BottomSheet from "./BottomSheet";
 import Button from "./Button";
 import Code from "./Code";
 import Header from "./Header";
+import { useKeyboardController } from "./KeyboardController";
 import Snippets from "./Snippets";
 import { useStore } from "./Store";
 
 export default function Container() {
   const { state, dispatch } = useStore();
+  const { ref } = useKeyboardController();
 
   return (
     <div className="v-screen h-screen">
@@ -18,13 +20,19 @@ export default function Container() {
       <Button
         className="fixed bottom-8 right-8 h-14 w-14 shadow-lg"
         variant="primary"
-        onClick={() => dispatch({ type: "toggleMode", mode: "command" })}
+        onClick={() => {
+          dispatch({ type: "toggleMode", mode: "command" });
+          ref.current?.blur();
+        }}
       >
         <MdCode size="1.5em" />
       </Button>
       <BottomSheet
         isActive={state.mode === "command"}
-        onClose={() => dispatch({ type: "toggleMode", mode: "view" })}
+        onClose={() => {
+          dispatch({ type: "toggleMode", mode: "view" });
+          ref.current?.blur();
+        }}
       >
         <Snippets />
       </BottomSheet>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import { MdKeyboard, MdKeyboardHide } from "react-icons/md";
 import { SiJavascript } from "react-icons/si";
+import { useKeyboardController } from "./KeyboardController";
 import { useStore } from "./Store";
 
 export default function Header() {
   const { state, dispatch } = useStore();
+  const { ref } = useKeyboardController();
 
   return (
     <div className="flex shadow-md px-4 py-2 inset-x-0 top-0 fixed bg-white z-30">
@@ -15,12 +17,21 @@ export default function Header() {
       <button
         key={state.mode}
         className="p-2"
-        onClick={() =>
-          dispatch({
-            type: "toggleMode",
-            mode: state.mode === "edit" ? "view" : "edit",
-          })
-        }
+        onClick={() => {
+          if (state.mode === "edit") {
+            dispatch({
+              type: "toggleMode",
+              mode: "view",
+            });
+            ref.current?.blur();
+          } else {
+            dispatch({
+              type: "toggleMode",
+              mode: "edit",
+            });
+            ref.current?.focus();
+          }
+        }}
       >
         {state.mode === "edit" ? (
           <MdKeyboardHide size="1.5em" />
